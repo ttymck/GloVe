9136d17b0188561c49d9bfbc8e98500d60a3a18e
Tyler Kontra <tyler@tylerkontra.com>
2019-02-23 12:54:46 -0800

    fix(verbosity): remove verbose feature

--- src/vocab_count.c
+++ src/vocab_count.c
@@ -45,7 +45,6 @@ typedef struct hashrec {
     struct hashrec *next;
 } HASHREC;
 
-int verbose = 2; // 0, 1, or 2
 long long min_count = 1; // min occurrences for inclusion in vocab
 long long max_vocab = 0; // max_vocab = 0 for no limit
 
@@ -174,7 +173,6 @@ int get_counts() {
     FILE *fid = stdin;
     
     fprintf(stderr, "BUILDING VOCABULARY\n");
-    if (verbose > 1) fprintf(stderr, "Processed %lld tokens.", i);
     // sprintf(format,"%%%ds",MAX_STRING_LENGTH);
     while ( ! feof(fid)) {
         // Insert all tokens into hashtable
@@ -185,9 +183,7 @@ int get_counts() {
             return 1;
         }
         hashinsert(vocab_hash, str);
-        if (((++i)%100000) == 0) if (verbose > 1) fprintf(stderr,"\033[11G%lld tokens.", i);
     }
-    if (verbose > 1) fprintf(stderr, "\033[0GProcessed %lld tokens.\n", i);
     vocab = malloc(sizeof(VOCAB) * vocab_size);
     for (i = 0; i < TSIZE; i++) { // Migrate vocab to array
         htmp = vocab_hash[i];
@@ -202,7 +198,6 @@ int get_counts() {
             htmp = htmp->next;
         }
     }
-    if (verbose > 1) fprintf(stderr, "Counted %lld unique words.\n", j);
     if (max_vocab > 0 && max_vocab < j)
         // If the vocabulary exceeds limit, first sort full vocab by frequency without alphabetical tie-breaks.
         // This results in pseudo-random ordering for words with same frequency, so that when truncated, the words span whole alphabet
@@ -212,13 +207,11 @@ int get_counts() {
     
     for (i = 0; i < max_vocab; i++) {
         if (vocab[i].count < min_count) { // If a minimum frequency cutoff exists, truncate vocabulary
-            if (verbose > 0) fprintf(stderr, "Truncating vocabulary at min count %lld.\n",min_count);
             break;
         }
         printf("%s %lld\n",vocab[i].word,vocab[i].count);
     }
     
-    if (i == max_vocab && max_vocab < j) if (verbose > 0) fprintf(stderr, "Truncating vocabulary at size %lld.\n", max_vocab);
     fprintf(stderr, "Using vocabulary of size %lld.\n\n", i);
     return 0;
 }
@@ -243,18 +236,15 @@ int main(int argc, char **argv) {
         printf("Simple tool to extract unigram counts\n");
         printf("Author: Jeffrey Pennington (jpennin@stanford.edu)\n\n");
         printf("Usage options:\n");
-        printf("\t-verbose <int>\n");
-        printf("\t\tSet verbosity: 0, 1, or 2 (default)\n");
         printf("\t-max-vocab <int>\n");
         printf("\t\tUpper bound on vocabulary size, i.e. keep the <int> most frequent words. The minimum frequency words are randomly sampled so as to obtain an even distribution over the alphabet.\n");
         printf("\t-min-count <int>\n");
         printf("\t\tLower limit such that words which occur fewer than <int> times are discarded.\n");
         printf("\nExample usage:\n");
-        printf("./vocab_count -verbose 2 -max-vocab 100000 -min-count 10 < corpus.txt > vocab.txt\n");
+        printf("./vocab_count -max-vocab 100000 -min-count 10 < corpus.txt > vocab.txt\n");
         return 0;
     }
-    
-    if ((i = find_arg((char *)"-verbose", argc, argv)) > 0) verbose = atoi(argv[i + 1]);
+
     if ((i = find_arg((char *)"-max-vocab", argc, argv)) > 0) max_vocab = atoll(argv[i + 1]);
     if ((i = find_arg((char *)"-min-count", argc, argv)) > 0) min_count = atoll(argv[i + 1]);
     return get_counts();

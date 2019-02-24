bacb5bbc1cd803c2ee6bf1899b3eb5514cc4081b
Tyler Kontra <46075030+ttymck@users.noreply.github.com>
2019-02-23 17:25:35 -0800

    Refactor: cooccur (#2)
    * fix(verbosity): noop
    
    * fix(verbosity): fix incomplete delete
    
    * fix(vocab_file): scopes vocab file
    
    * fix(scope): scopes max_product and overflow_length
    
    * fix(scope): scopes window_size, symmetric
    
    * fix(scope): scopes memory_limit, distance_weighting
    
    * feat(wrapper): adds cooccur to wrapper

--- wrapper.py
+++ wrapper.py
@@ -6,12 +6,24 @@ CWD = Path(__file__).parent
 def vocab_count(corpus: Path, output_path: Path):
     program = CWD / "build" / "vocab_count"
     with open(corpus, "r") as infile, open(output_path, "w") as outfile:
-        subprocess.Popen(str(program), stdin=infile, stdout=outfile)
+        p = subprocess.Popen(str(program), stdin=infile, stdout=outfile)
+        p.wait()
     return output_path
+
+def cooccur(corpus: Path, output_path: Path, **kwargs):
+    if kwargs:
+        print("kwargs!")
+
+    program = CWD / "build" / "cooccur"
+    with open(corpus, "r") as infile, open(output_path, "w") as outfile:
+        p = subprocess.Popen(str(program), stdin=infile, stdout=outfile)
+        p.wait()
 
 if __name__ ==  "__main__":
     corpus = CWD / "text8"
-    outfile = CWD / "vocab_py.txt"
+    vocab = CWD / "vocab_py.txt"
+    cobin = CWD / "cooccurrences.bin.feat_py"
 
-    o = vocab_count(corpus, outfile)
+    o = vocab_count(corpus, vocab)
+    c = cooccur(corpus, cobin)
     print("file ready at {}".format(o))

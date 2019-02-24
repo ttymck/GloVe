f2b47ad5d17b1ffcff26ca228fa9894314d30c7e
Tyler Kontra <tyler@tylerkontra.com>
2019-02-23 17:35:15 -0800

    fix(verbosity): remove verbose flag

--- src/shuffle.c
+++ src/shuffle.c
@@ -36,7 +36,6 @@ typedef struct cooccur_rec {
     real val;
 } CREC;
 
-int verbose = 2; // 0, 1, or 2
 long long array_size = 2000000; // size of chunks to shuffle individually
 char *file_head; // temporary file string
 real memory_limit = 2.0; // soft limit, in gigabytes
@@ -95,7 +94,6 @@ int shuffle_merge(int num) {
             return 1;
         }
     }
-    if (verbose > 0) fprintf(stderr, "Merging temp files: processed %ld lines.", l);
     
     while (1) { //Loop until EOF in all files
         i = 0;
@@ -112,7 +110,6 @@ int shuffle_merge(int num) {
         l += i;
         shuffle(array, i-1); // Shuffles lines between temp files
         write_chunk(array,i,fout);
-        if (verbose > 0) fprintf(stderr, "\033[31G%ld lines.", l);
     }
     fprintf(stderr, "\033[0GMerging temp files: processed %ld lines.", l);
     for (fidcounter = 0; fidcounter < num; fidcounter++) {
@@ -135,20 +132,17 @@ int shuffle_by_chunks() {
     array = malloc(sizeof(CREC) * array_size);
     
     fprintf(stderr,"SHUFFLING COOCCURRENCES\n");
-    if (verbose > 0) fprintf(stderr,"array size: %lld\n", array_size);
     sprintf(filename,"%s_%04d.bin",file_head, fidcounter);
     fid = fopen(filename,"w");
     if (fid == NULL) {
         fprintf(stderr, "Unable to open file %s.\n",filename);
         return 1;
     }
-    if (verbose > 1) fprintf(stderr, "Shuffling by chunks: processed 0 lines.");
     
     while (1) { //Continue until EOF
         if (i >= array_size) {// If array is full, shuffle it and save to temporary file
             shuffle(array, i-2);
             l += i;
-            if (verbose > 1) fprintf(stderr, "\033[22Gprocessed %ld lines.", l);
             write_chunk(array,i,fid);
             fclose(fid);
             fidcounter++;
@@ -167,8 +161,7 @@ int shuffle_by_chunks() {
     shuffle(array, i-2); //Last chunk may be smaller than array_size
     write_chunk(array,i,fid);
     l += i;
-    if (verbose > 1) fprintf(stderr, "\033[22Gprocessed %ld lines.\n", l);
-    if (verbose > 1) fprintf(stderr, "Wrote %d temporary file(s).\n", fidcounter + 1);
+
     fclose(fid);
     free(array);
     return shuffle_merge(fidcounter + 1); // Merge and shuffle together temporary files
@@ -196,8 +189,6 @@ int main(int argc, char **argv) {
         printf("Tool to shuffle entries of word-word cooccurrence files\n");
         printf("Author: Jeffrey Pennington (jpennin@stanford.edu)\n\n");
         printf("Usage options:\n");
-        printf("\t-verbose <int>\n");
-        printf("\t\tSet verbosity: 0, 1, or 2 (default)\n");
         printf("\t-memory <float>\n");
         printf("\t\tSoft limit for memory consumption, in GB; default 4.0\n");
         printf("\t-array-size <int>\n");
@@ -206,11 +197,10 @@ int main(int argc, char **argv) {
         printf("\t\tFilename, excluding extension, for temporary files; default temp_shuffle\n");
         
         printf("\nExample usage: (assuming 'cooccurrence.bin' has been produced by 'coccur')\n");
-        printf("./shuffle -verbose 2 -memory 8.0 < cooccurrence.bin > cooccurrence.shuf.bin\n");
+        printf("./shuffle -memory 8.0 < cooccurrence.bin > cooccurrence.shuf.bin\n");
         return 0;
     }
    
-    if ((i = find_arg((char *)"-verbose", argc, argv)) > 0) verbose = atoi(argv[i + 1]);
     if ((i = find_arg((char *)"-temp-file", argc, argv)) > 0) strcpy(file_head, argv[i + 1]);
     else strcpy(file_head, (char *)"temp_shuffle");
     if ((i = find_arg((char *)"-memory", argc, argv)) > 0) memory_limit = atof(argv[i + 1]);
